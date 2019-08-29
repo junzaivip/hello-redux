16e53a53befea8716875840b911c7486a45e6946
junzaivip <goxuexi@qq.com>
2019-08-29 11:21:57 +0800

    整理目录结构

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { createStore } from 'redux'
 import reducer from './reducers/counter'
 
+import {increment,decrement} from './actions';
+
 const store = createStore(reducer)
 
 // store.subscribe(()=> console.log("state updated!",store.getState()));
@@ -21,8 +23,8 @@ const store = createStore(reducer)
 
 const render = ()=>{
     ReactDOM.render(<App
-        onIncrement={ ()=> store.dispatch({ type:"INCREMENT" }) }
-        onDecrement ={ ()=> store.dispatch({ type:"DECREMENT"})  }
+        onIncrement={ ()=> store.dispatch(increment()) }
+        onDecrement ={ ()=> store.dispatch(decrement())  }
         value = {store.getState()}/>, document.getElementById('root'));
 }
 
